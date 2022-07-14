cab1abf91e3e1d64a1b480f0cbc990b7c0876b07
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-07-13 22:51:09 -0300

    v0.2.0 criação dos arquivos de footer, header, home e login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import './App.css';
+import Home from './components/Home';
+import Login from './components/login/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
