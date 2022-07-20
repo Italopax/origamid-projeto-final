ea0f9993a2b33f1afdf1f4902f16fe9b3bd4693c
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-07-19 23:23:39 -0300

    v0.6.0 criação de um arquivo para conter as informações dos end points, criação de um custom hook para os inputs dos formulários, e criação de funções assíncronas no LoginForm para enviar dados de login e puxar dados do usuário

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,35 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { TOKEN_POST, USER_GET } from "../../api";
+import useForm from "../../hooks/useForm";
 import Button from "../forms/Button";
 import Input from "../forms/Input";
 
 const LoginForm = ()=>{
 
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const username = useForm(); 
+  const password = useForm(); 
 
-  function handleSubmit (event) {
-    event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({username, password})
-    }).then(res => {
-      console.log(res)
-      return res.json();
-    }).then(json => {
-      console.log(json)
-    })
+  React.useEffect(()=>{
+    const token = window.localStorage.getItem('token');
+    if(token) getUser(token);
+  })
+
+  async function getUser (token) {
+    const {url, options} = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json)
   }
 
+  async function handleSubmit (event) {
+    event.preventDefault();
+
+    if(username.validate() && password.validate()){
+      const {url, options} = TOKEN_POST({
+        username: username.value,
+        password: password.value
+      });
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
+    }
+  }
+  
   return(
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="passoword" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="passoword" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
