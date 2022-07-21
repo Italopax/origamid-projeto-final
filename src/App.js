ec1f02a3a0e8fba75b813702b3b77698eb23f99f
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-07-20 23:24:53 -0300

    v0.7.0 criação e utilização do contexto da aplicação

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Login from './components/login/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
