4fef7cc927ccb235ddd97658fbcfd48bd5a67eeb
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-07-28 23:58:34 -0300

    v0.12.0 criando ProtectRouter.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/Home';
 import Login from './components/login/Login';
+import User from './components/user/User';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserStorage } from './UserContext';
+import ProtectedRouter from './components/helper/ProtectedRouter';
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRouter path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
