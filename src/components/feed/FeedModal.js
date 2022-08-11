b17fbbff0915fc2b16df3b579f1eced7314ad539
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-08-11 14:59:52 -0300

    v0.18.0 fim da criação da modal das postagens

--- src/components/feed/FeedModal.js
+++ src/components/feed/FeedModal.js
@@ -6,7 +6,7 @@ import Error from "../helper/Error";
 import Loading from "../helper/Loading";
 import PhotoContent from "../photo/PhotoContent";
 
-const FeedModal = ({photo})=>{
+const FeedModal = ({photo, setModalPhoto})=>{
 
   const {data, loading, error, request} = useFetch();
 
@@ -15,9 +15,12 @@ const FeedModal = ({photo})=>{
     request(url, options);
   }, [photo, request])
 
+  function handleOutsideClick (e){
+    if(e.target === e.currentTarget) setModalPhoto(null);
+  }
 
   return(
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} /> }
       {loading && <Loading /> }
       {data && <PhotoContent data={data} />}
